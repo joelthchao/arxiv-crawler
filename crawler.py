9faff958bc3f01af2e4c2cdca0a78a26a2a7c725
Joel Chao <joelthchao@gmail.com>
2016-12-04 22:21:08 +0800

    improve style

--- crawler.py
+++ crawler.py
@@ -30,8 +30,8 @@ CREATE TABLE IF NOT EXISTS Publications (
 ''')
 
 for field in fields:
-    for year in ['16']: #years:
-        for month in ['02']: #months:
+    for year in years:
+        for month in months:
             query_url = url.format(field, year, month)
             print 'Retrieving {}'.format(query_url)
             uh = urllib.urlopen(query_url)
@@ -40,21 +40,20 @@ for field in fields:
             titles = soup.findAll('div', {'class': 'list-title'})
             authors = soup.findAll('div', {'class': 'list-authors'})
             paper_urls = soup.findAll('span', {'class': 'list-identifier'})
-            #[0].findAll('a')[0].string
             if len(titles) != len(authors):
                 print 'number of titles and authors mismatch'
             else:
-                for i in range(len(titles)):
-                    title = titles[i].contents[-1].strip()
-                    paper_url = 'http://arxiv.org' + paper_urls[i].contents[0].attrs[0][1]
+                for title, author, paper_url in zip(titles, authors, paper_urls):
+                    title = title.contents[-1].strip()
+                    paper_url = 'http://arxiv.org' + paper_url.contents[0].attrs[0][1]
                     cur.execute('''
                         INSERT OR IGNORE INTO Papers (title, url, year, month) 
                         VALUES (?, ?, ?, ?)''', (title, paper_url, int(year), int(month)))
                     cur.execute('SELECT id FROM Papers WHERE title = ? ', (title, ))
                     paper_id = cur.fetchone()[0]
                     
-                    author = [author.string.strip() for author in authors[i].findAll('a')]
-                    for name in author:
+                    paper_authors = [au.string.strip() for au in author.findAll('a')]
+                    for name in paper_authors:
                         cur.execute('''
                             INSERT OR IGNORE INTO Authors (name) 
                             VALUES (?)''', (name, ))
